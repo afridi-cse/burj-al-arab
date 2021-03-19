3650f43e242d1cf77c04f3e608a36aee05e75603
Shahid Afridi <shahidafridi.cse@gmail.com>
2021-03-20 04:57:42 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA2Opd_fCjE8z5XxiTIihPiCiAg-yCPErc",
+    authDomain: "burj-al-arab-afridi.firebaseapp.com",
+    projectId: "burj-al-arab-afridi",
+    storageBucket: "burj-al-arab-afridi.appspot.com",
+    messagingSenderId: "710328371161",
+    appId: "1:710328371161:web:7e283c9c3f357529fcfbc8"
+  };
+  export default firebaseConfig;
